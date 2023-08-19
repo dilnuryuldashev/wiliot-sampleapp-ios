a5c94560aac781fe1dcf07bb5de6c47aa6ded986
dilnuryuldashev <dilnuryuldashev@gmail.com>
2023-08-19 00:51:59 -0400

    Before @_implementationOnly addition

--- WiliotGatewayBLEConnection/Sources/WiliotGatewayBLEConnection/WiliotGatewayBLEConnectionBridge.swift
+++ WiliotGatewayBLEConnection/Sources/WiliotGatewayBLEConnection/WiliotGatewayBLEConnectionBridge.swift
@@ -1,7 +1,10 @@
 import Foundation
 
+//old function def: public func initialize(appToken: String, ownerID: String)
 @_cdecl("initialize")
-public func initialize(appToken: String, ownerID: String) {
+public func initialize() {
+    let appToken = "MmE1NjQ0MDctMmQ5Yy00NWJjLTk2MzktNjE1ZjUzM2QxZjBiOkxZU2ZIc0pzQjJiNG8yVS1oNTlBM1h1VGUtd3ZFY3A5SGgtZnpQaHB0TnM=" //"NDZmNTc3ODItYzg1NC00ZGM2LTk5NzctMzdlZWMyZDgzZTVmOkR5VHNJV2JzeXJwWW9Ic0hUcjFMSHpGZVRaVG14RTh2cmU3dGFNZ21oRlk="
+    let ownerID = "201409513381"//"947302316108"
     WiliotGatewayBLEConnection.initialize(appToken: appToken, ownerID: ownerID)
 }
 
